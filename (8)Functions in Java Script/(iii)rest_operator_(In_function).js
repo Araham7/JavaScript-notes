9258a8b5c98ee9c8907aef6f021ad0b084448450
Araham Abeddin <108493882+Araham7@users.noreply.github.com>
2024-07-05 15:22:57 +0530

    Update (iii)rest_operator_(In_function).js

--- (8)Functions in Java Script/(iii)rest_operator_(In_function).js
+++ (8)Functions in Java Script/(iii)rest_operator_(In_function).js
@@ -4,6 +4,9 @@ function calculateCardPrice(num1 , num2 , ...num3){
     return num3
 }
 console.log(calculateCardPrice(200, 400, 500, 2000, 5120, 1098));
+// OutPut : The value of 'num1' = 200 , 'num2' = 400 and 'num3' = 500,2000,5120,1098
+[ 500, 2000, 5120, 1098 ]
+
 /* 
 NOTE :
 ***** "calculateCardPrice" function "hamare dwara diye gaye argument(200, 400, 500, 2000, 5120, 1098)" mese ("num1" me 200 ko store karwayega) , ("num2" me 400 ko store karwayega) aur ("num3" me array ke form me [500, 2000, 5120, 1098] ko store karliya jata hai.)
