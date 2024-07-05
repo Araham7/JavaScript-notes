5bf1208e01ff6f154a38e079866728451d364d16
Araham Abeddin <108493882+Araham7@users.noreply.github.com>
2024-07-05 15:37:35 +0530

    Update (1)for_loop.js

--- (11)(Loops) Ittaration Js/(1)for_loop.js
+++ (11)(Loops) Ittaration Js/(1)for_loop.js
@@ -53,7 +53,8 @@ for (let i = 1; i <= 10; i++) {
     // console.log(`Outer loop(3) value: ${i}`);
    for (let j = 1; j <= 10; j++) {
     // console.log(`Inner loop(3) value ${j} and inner loop ${i}`);
-    console.log(i + '*' + j + ' = ' + i*j );
+    // console.log(i + '*' + j + ' = ' + i*j );
+       console.log(`${i} * ${j}  = ${i*j}`);
    }
 }
 /* -----------------------
